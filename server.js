52554eb1a4d6db0332d55ed3ee3b662540856c3f
aayushsapkota01 <sapkotaaayush10@gmail.com>
2023-01-18 18:42:38 -0500

    initial commit 2

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+/*********************************************************************************
+ * WEB322 – Assignment 1
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ * (including web sites) or distributed to other students. *
+ * Name: Aayush Sapkota Student ID: 152406211 Date: Jan 18, 2023 *
+ * Online (Cyclic) URL: _______________________________________________________
+ * ********************************************************************************/
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
