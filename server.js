640a367b4e8e983cd563964686879c55700935ac
aayushsapkota01 <sapkotaaayush10@gmail.com>
2023-01-18 18:47:09 -0500

    updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * No part of this assignment has been copied manually or electronically from any other source
  * (including web sites) or distributed to other students. *
  * Name: Aayush Sapkota Student ID: 152406211 Date: Jan 18, 2023 *
- * Online (Cyclic) URL: _______________________________________________________
+ * Online (Cyclic) URL: https://sore-red-cygnet-wear.cyclic.app
  * ********************************************************************************/
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
@@ -12,7 +12,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Aayush Sapkota - 152406211");
 });
 
 // setup http server to listen on HTTP_PORT
